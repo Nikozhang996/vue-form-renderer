45695bb701b31d21ddcf6598768f984e4c98320a
Niko <593177876@qq.com>
2020-06-17 18:43:52 +0800

    chore: change webpack config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from "vue";
+// import Vue from "vue";
 import App from "./App.vue";
-import ElementUI from "element-ui";
-import "./styles.scss";
+// import ElementUI from "element-ui";
+const Vue = require("vue");
+const ElementUI = require("element-ui");
+// import "./styles.scss";
 
 Vue.use(ElementUI);
 
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    // entry: "./src/components/FormRenderer/index.vue",
+    // output: {
+    //   path: path.resolve(__dirname, "dist"),
+    //   filename: "vue-form-renderer.js"
+    // },
+    externals: {
+      vue: "Vue",
+      "core-js": "core-js",
+      "element-ui": "ElementUI"
+    }
+  }
+};
